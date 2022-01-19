84fd0412c6329abe080fb090426ff3f52358981e
alandionei <dioneialpha@gmail.com>
2022-01-19 00:12:52 -0300

    Êstudos de Robot Framework QANinja

---
+++ robot/app.py
@@ -0,0 +1,8 @@
+
+
+
+def welcome(name):
+    return "Olá " + name + ", bem vindo ao curso básico de robot framework!"
+
+    result = welcome("Alan")
+    print(result)
---
+++ robot/test.robot
@@ -0,0 +1,8 @@
+
+***Settings ***
+Library      app.py
+
+
+*** Test Cases ***
+deve retornar mensagem de boas vindas
+  welcome       Alan
